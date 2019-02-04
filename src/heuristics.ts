f46dce6605f1c22053f8622b07f56ab3d194b545
Srishan Bhattarai <srishanbhattarai@gmail.com>
2019-02-04 22:36:21 +0545

    Add Sorenson Dice coefficient heuristic
    * This string similarity heuristic seems to be more accurate than
    something like Levenshtein distance and works by checking the length
    of bigrams in any two strings.

---
+++ src/heuristics.ts
@@ -0,0 +1,52 @@
+/**
+ * Computes the Sørensen–Dice coefficient of two strings.
+ *  (Ref. https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient)
+ *
+ * In addition to the usual computation, we alter the input strings by removing
+ * whitespace.
+ * 
+ * @param {string} first
+ * @param {string} second
+ * @returns {number}
+ */
+export function sorensenDiceCoefficient(first: string, second: string): number {
+  const x = first.replace(/\s+/g, '')
+  const y = second.replace(/\s+/g, '')
+
+  const xBigrams = ngrams(2, x)
+  const yBigrams = ngrams(2, y)
+  const tBigrams = new Set([...xBigrams].filter(x => yBigrams.has(x)));
+
+  const num = 2 * tBigrams.size
+  const denom = xBigrams.size + yBigrams.size
+
+  return num / denom
+}
+
+/**
+ * Compute the n-grams of any string.
+ * 
+ * @param {number} n - Value of "n". Example: n = 2 computes the bigrams.
+ * @param {string} str - The string to compute the n-gram for.
+ */
+export function ngrams(n: number, str: string): Set<String> {
+  const strlen = str.length
+
+  // If "n" equals or exceeds the length of the string, then it is the only ngram.
+  // Also, if "n" is negative, the input is spurious, return the string.
+  if (n >= strlen || n < 0) {
+    return new Set<String>([str])
+  }
+
+  const results = new Set<String>()
+
+  let idx = 0
+  while (idx < (strlen - n + 1)) {
+    const ngram = str.slice(idx, idx + n)
+    results.add(ngram)
+
+    idx += 1
+  }
+
+  return results
+}
---
+++ src/tests/heuristics.test.ts
@@ -0,0 +1,23 @@
+import { sorensenDiceCoefficient, ngrams } from '../heuristics'
+
+describe('Sørensen–Dice coefficient', () => {
+    it('should correctly compute the coefficient', () => {
+        expect(sorensenDiceCoefficient('night', 'nacht')).toEqual(0.25)
+    })
+})
+
+describe('N-grams', () => {
+    it('should correctly compute the ngrams', () => {
+        expect(ngrams(2, 'night')).toEqual(new Set(['ni', 'ig', 'gh', 'ht']))
+        expect(ngrams(2, 'ab')).toEqual(new Set(['ab']))
+        expect(ngrams(3, 'word')).toEqual(new Set(['wor', 'ord']))
+    })
+
+    it('should handle cases where "n > str.length"', () => {
+      expect(ngrams(5, 'xy')).toEqual(new Set(['xy']))
+    })
+
+    it('should handle cases where "n" is negative', () => {
+      expect(ngrams(-1, 'xy')).toEqual(new Set(['xy']))
+    })
+})
